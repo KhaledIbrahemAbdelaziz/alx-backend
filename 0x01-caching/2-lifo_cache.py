4c57c5c4349fc992bc732403c3499912b0bda7fc
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2024-01-03 21:34:14 -0800

    Task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""Task 2"""
+"""Last-In First-Out caching module.
+"""
 from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """Representing Stack Caching"""
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
         """Initializes the cache.
         """
@@ -20,8 +24,8 @@ class LIFOCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                keydiscard, _ = self.cache_data.popitem(True)
-                print("DISCARD:", keydiscard)
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
