56d0f2ba7136fb13d7fad2e17732525670f9a7f5
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2024-01-03 21:32:45 -0800

    Task 1

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Task 2"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Representing Stack Caching"""
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                keydiscard, _ = self.cache_data.popitem(True)
+                print("DISCARD:", keydiscard)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
